b5391591511e7c9ffad24096401a1b884463fd1b
BelyavskiyIgor <belyavskiyi@gmail.com>
2022-07-16 20:19:27 +0300

    Initial commit

---
+++ src/test/java/ru/belyavskiy/skills/SkillsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.belyavskiy.skills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
